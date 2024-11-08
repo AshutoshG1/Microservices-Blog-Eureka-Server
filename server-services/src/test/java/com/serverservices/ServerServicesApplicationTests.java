fb8a8aa2f269ca3dab0857f1d56349c2e3eae18d
AshutoshG1 <ashutoshgautamspn@gmail.com>
2024-11-08 15:39:42 +0530

    committed

---
+++ server-services/src/test/java/com/serverservices/ServerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
